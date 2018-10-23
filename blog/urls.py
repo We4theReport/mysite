0f57e3178b55fb4b35fa90ab6304bfa6655ee3e4
We4theReport <alessandro.mastrelli@hotmail.com>
2018-10-23 16:49:22 +0200

    Redefine the django library

--- blog/urls.py
+++ blog/urls.py
@@ -1,6 +1,6 @@
-from django.conf.urls import url
+from django.urls import path
 from . import views
 
 urlpatterns = [
-    url(r'^$', view.post_list, name='post_list'),
+    path('', view.post_list, name='post_list'),
 ]
